4e8178d651b11094f36e5f4beb04be8634ea6bbe
manas887 <116465782+manas887@users.noreply.github.com>
2023-02-24 23:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let todoInput = document.getElementById('newTodoInput');
+let todoList = document.getElementById('todoList');
 
+function addTodo() {
+  let todoText = todoInput.value;
+  if (todoText.trim() === '') {
+    alert('Please enter a valid todo item');
+    return;
+  }
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode(todoText));
+  todoList.appendChild(li);
+  todoInput.value = '';
+}
